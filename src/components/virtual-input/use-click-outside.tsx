db59fce738faad2d28add8ced10581a3094878d7
yangshuning.ysn <yangshuning.ysn@antgroup.com>
2026-03-12 17:19:18 +0800

    fix: update active-detect

--- src/components/virtual-input/use-click-outside.tsx
+++ src/components/virtual-input/use-click-outside.tsx
@@ -1,17 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 // 监听点击组件外部的事件
 function useClickOutside(
   handler: (event: MouseEvent) => void,
   ref: React.RefObject<HTMLElement>,
-  hasKeyboardProps: boolean = false
+  hasKeyboardProps = false
 ) {
+  const handlerRef = useRef(handler)
+
+  useEffect(() => {
+    handlerRef.current = handler
+  }, [handler])
+
   useEffect(() => {
     function handleClick(event: MouseEvent) {
       if (!ref.current || ref.current.contains(event.target as Node)) {
         return
       }
-      handler(event)
+      if (ref.current?.contains(document.activeElement)) {
+        return
+      }
+      handlerRef.current(event) // 使用 ref 中的 handler
     }
 
     // 向前兼容逻辑：
@@ -21,20 +30,12 @@ function useClickOutside(
     //      这种情况通常是 VirtualInput + NumberKeyboard 为兄弟关系，在以前版本中点击 NumberKeyboard **不会**触发 VirtualInput 的 blur 事件
     //      原先原理：通过 NumberKeyboard 内部 onMouseDown 时 preventDefault 阻止的 VirtualInput 内原生的 blur 事件
     //      新的原理：NumberKeyboard 的 Popup 默认会 stopPropagation click, 这里在冒泡阶段监听不到，不会调用 VirtualInput 的 onBlur 回调（非原生事件）。
-    document.addEventListener(
-      'click',
-      handleClick,
-      hasKeyboardProps ? true : false
-    )
 
+    document.addEventListener('click', handleClick, hasKeyboardProps)
     return () => {
-      document.removeEventListener(
-        'click',
-        handleClick,
-        hasKeyboardProps ? true : false
-      )
+      document.removeEventListener('click', handleClick, hasKeyboardProps)
     }
-  }, [handler, ref])
+  }, [ref]) // 只依赖 ref，不依赖 handler
 }
 
 export default useClickOutside
